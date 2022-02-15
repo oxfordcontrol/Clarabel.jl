3e1a8b50f8a25bf937cc882d32da35f19569ce57
Paul Goulart <paul.goulart@eng.ox.ac.uk>
2022-02-15 11:37:04 +0000

    kwdef settings

--- prototype/settings.jl
+++ prototype/settings.jl
@@ -1,72 +1,33 @@
 # -------------------------------------
 # User definable settings
 # -------------------------------------
-mutable struct Settings{T <: AbstractFloat}
+Base.@kwdef mutable struct Settings{T <: AbstractFloat}
 
-    max_iter::DefaultInt
-    verbose::Bool
-    tol_gap_abs::T
-    tol_gap_rel::T
-    tol_feas::T
-    direct_kkt_solver::Bool
+    max_iter::DefaultInt    = 50
+    verbose::Bool           = true
+    tol_gap_abs::T          = 1e-7
+    tol_gap_rel::T          = 1e-6
+    tol_feas::T             = 1e-5
+    direct_kkt_solver::Bool = true
 
-    max_step_fraction::T
+    max_step_fraction::T    = 0.99
 
     #static regularization parameters
-    static_regularization_enable::Bool
-    static_regularization_eps::T
+    static_regularization_enable::Bool  = true
+    static_regularization_eps::T        = 1e-8
 
     #dynamic regularization parameters
-    dynamic_regularization_enable::Bool
-    dynamic_regularization_eps::T
-    dynamic_regularization_delta::T
+    dynamic_regularization_enable::Bool = true
+    dynamic_regularization_eps::T       = 1e-12
+    dynamic_regularization_delta::T     = 1e-7
 
     #iterative refinement
-    iterative_refinement_enable::Bool
-    iterative_refinement_reltol::T
-    iterative_refinement_abstol::T
-    iterative_refinement_max_iter::Int
-    iterative_refinement_halt_ratio::T
+    iterative_refinement_enable::Bool   = true
+    iterative_refinement_reltol::T      = 1e-10
+    iterative_refinement_abstol::T      = 1e-10
+    iterative_refinement_max_iter::Int  = 10
+    iterative_refinement_halt_ratio::T  = 2
 
-    function Settings{T}(;
-
-        max_iter = 50,
-        verbose = true,
-        tol_gap_abs = 1e-7,
-        tol_gap_rel = 1e-6,
-        tol_feas    = 1e-5,
-        direct_kkt_solver = true,
-        max_step_fraction = 0.99,
-        #
-        static_regularization_enable = true,
-        static_regularization_eps = 1e-8,
-        #
-        dynamic_regularization_enable = true,
-        dynamic_regularization_eps = 1e-12,
-        dynamic_regularization_delta = 1e-7,
-        #
-        iterative_refinement_enable = true,
-        iterative_refinement_reltol = 1e-10,
-        iterative_refinement_abstol = 1e-10,
-        iterative_refinement_max_iter = 10,
-        iterative_refinement_halt_ratio = 2.) where {T}
-
-        new(max_iter,verbose,
-        tol_gap_abs,tol_gap_rel,tol_feas,
-        direct_kkt_solver,max_step_fraction,
-        static_regularization_enable,
-        static_regularization_eps,
-        dynamic_regularization_enable,
-        dynamic_regularization_eps,
-        dynamic_regularization_delta,
-        iterative_refinement_enable,
-        iterative_refinement_reltol,
-        iterative_refinement_abstol,
-        iterative_refinement_max_iter,
-        iterative_refinement_halt_ratio
-        )
-
-    end
 end
 
 # Default to DefaultFloat type for reals
