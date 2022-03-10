f0d6ce207e3090364efc90f9d4e7223fcef94ddd
Paul Goulart <pgoulart@engs-27031.local>
2022-03-10 20:51:47 +0000

    fix docs build

--- docs/patch_packages.jl
+++ docs/patch_packages.jl
@@ -1,4 +1,3 @@
 @info "Temporary import of dev branches in supporting packages"
-using Pkg
-Pkg.activate("Clarabel")
 Pkg.add(url="https://github.com/osqp/QDLDL.jl",rev="6bb6bf40565fd69cacf21936fa1ca50b5e06c87a")
+Pkg.pin("QDLDL")
