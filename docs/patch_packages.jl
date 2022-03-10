3f05dfbe0a5355c9eebeedc9a1d039e8d9bcd87d
Paul Goulart <pgoulart@engs-27031.local>
2022-03-10 20:59:46 +0000

    fix docs build

--- docs/patch_packages.jl
+++ docs/patch_packages.jl
@@ -1,4 +1,4 @@
 @info "Temporary import of dev branches in supporting packages"
 using Pkg
-Pkg.add(url="https://github.com/osqp/QDLDL.jl",rev="6bb6bf40565fd69cacf21936fa1ca50b5e06c87a")
+Pkg.add(url="https://github.com/osqp/QDLDL.jl",rev="dev-0.2.0")
 Pkg.pin("QDLDL")
