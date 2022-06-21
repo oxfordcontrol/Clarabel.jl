5f3734e80057469068d9902a503930d5582d1403
lalinc4817 <lalinc4817@ENGS-28274.eng.ox.ac.uk>
2022-06-21 16:16:19 +0100

    merge static refinement, minor change

--- src/kktsolvers/direct-ldl/directldl_qdldl.jl
+++ src/kktsolvers/direct-ldl/directldl_qdldl.jl
@@ -35,14 +35,6 @@ function update_values!(
     values::Vector{T}
 ) where{T,Ti}
 
-<<<<<<< HEAD
-        #Updating values in both the KKT matrix and
-        #in the reordered copy held internally by QDLDL.
-        #The former is needed for iterative refinement since
-        #QDLDL does not have internal iterative refinement
-        QDLDL.update_values!(ldlsolver.factors,index,values)
-        @views ldlsolver.KKT.nzval[index] .= values
-=======
     #Update values that are stored within
     #the reordered copy held internally by QDLDL.
 
@@ -54,7 +46,6 @@ function update_values!(
     #would be needed).   Need to test how slow a
     #complete permuted updated would be though
     QDLDL.update_values!(ldlsolver.factors,index,values)
->>>>>>> pg/static_refinement
 
 end
 
@@ -69,10 +60,6 @@ function offset_values!(
 ) where{T}
 
     QDLDL.offset_values!(ldlsolver.factors, index, offset, signs)
-<<<<<<< HEAD
-    @views @. ldlsolver.KKT.nzval[index] += offset*signs
-=======
->>>>>>> pg/static_refinement
 
 end
 
@@ -97,60 +84,5 @@ function solve!(
     x .= b
     QDLDL.solve!(ldlsolver.factors,x)
 
-<<<<<<< HEAD
-    if(settings.iterative_refinement_enable)
-        iterative_refinement(ldlsolver,x,b,settings)
-    end
-
-    return nothing
-end
-
-
-function iterative_refinement(ldlsolver::QDLDLDirectLDLSolver{T},x,b,settings) where{T}
-
-    work = ldlsolver.work
-
-    #iterative refinement params
-    IR_reltol    = settings.iterative_refinement_reltol
-    IR_abstol    = settings.iterative_refinement_abstol
-    IR_maxiter   = settings.iterative_refinement_max_iter
-    IR_stopratio = settings.iterative_refinement_stop_ratio
-
-    #Note that K is only triu data, so need to
-    #be careful when computing the residual here
-    K      = ldlsolver.KKT
-    KKTsym = ldlsolver.KKTsym
-    lastnorme = Inf
-
-    normb = norm(b,Inf)
-
-    for i = 1:IR_maxiter
-
-        #this is work = error = b - Kξ
-        work .= b
-        mul!(work,KKTsym,x,-1.,1.)
-        norme = norm(work,Inf)
-
-        # test for convergence before committing
-        # to a refinement step
-        if(norme <= IR_abstol + IR_reltol*normb)
-            return
-        end
-
-        #if we haven't improved by at least the halting
-        #ratio since the last pass through, then abort
-        if(lastnorme/norme < IR_stopratio)
-            return
-        end
-
-        #make a refinement and continue
-        QDLDL.solve!(ldlsolver.factors,work)     #this is Δξ
-        x .+= work
-
-        lastnorme = norme
-    end
-
-=======
->>>>>>> pg/static_refinement
     return nothing
 end
