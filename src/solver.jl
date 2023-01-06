bd5154f2489a033f197a1f597948be31225fc38a
Yuwen Chen <aaronchenyuwen@gmail.com>
2023-01-06 15:55:19 +0000

    Add the maxvolume example

--- src/solver.jl
+++ src/solver.jl
@@ -337,21 +337,21 @@ function solver_default_start!(s::Solver{T}) where {T}
     # If there are only symmetric cones, use CVXOPT style initilization
     # Otherwise, initialize along central rays
 
-    if (is_symmetric(s.cones))
-        #set all scalings to identity (or zero for the zero cone)
-        set_identity_scaling!(s.cones)
-        #Refactor
-        kkt_update!(s.kktsystem,s.data,s.cones)
-        #solve for primal/dual initial points via KKT
-        kkt_solve_initial_point!(s.kktsystem,s.variables,s.data)
-        #fix up (z,s) so that they are in the cone
-        variables_symmetric_initialization!(s.variables, s.cones)
+    # if (is_symmetric(s.cones))
+    #     #set all scalings to identity (or zero for the zero cone)
+    #     set_identity_scaling!(s.cones)
+    #     #Refactor
+    #     kkt_update!(s.kktsystem,s.data,s.cones)
+    #     #solve for primal/dual initial points via KKT
+    #     kkt_solve_initial_point!(s.kktsystem,s.variables,s.data)
+    #     #fix up (z,s) so that they are in the cone
+    #     variables_symmetric_initialization!(s.variables, s.cones)
 
-    else
-        #Assigns unit (z,s) and zeros the primal variables 
-        variables_unit_initialization!(s.variables, s.cones)
-    end
-
+    # else
+    #     #Assigns unit (z,s) and zeros the primal variables 
+    #     variables_unit_initialization!(s.variables, s.cones)
+    # end
+    variables_unit_initialization!(s.variables, s.cones)
     return nothing
 end
 
---
+++ test/maxvolume.jl
@@ -0,0 +1,84 @@
+using LinearAlgebra, SparseArrays
+using JuMP,Mosek,MosekTools
+using JLD,JLD2
+# include(".\\..\\src\\Clarabel.jl")
+using Clarabel
+using Hypatia
+
+"""
+Maximum volume hypercube} from Hypatia.jl,
+
+https://github.com/chriscoey/Hypatia.jl/tree/master/examples/maxvolume,
+"""
+
+n = 200
+# ensure there will be a feasible solution
+x = randn(n)
+A = sparse(Symmetric(sprand(n,n,5.0/n))) + 10I
+gamma = norm(A * x) / sqrt(n)
+freq = ones(n)
+freq ./= n
+
+
+#Result from Clarabel's generalized power cone
+println("generalized power cones via Clarabel")
+model = Model(Clarabel.Optimizer)
+@variable(model, x[1:n])
+@variable(model,z[1:n-1])
+@objective(model, Max, z[end])
+# trnasform a general power cone into a product of 3x3 power cones
+power = freq[1] + freq[2]
+@constraint(model, vcat(x[2],x[1],z[1]) in MOI.PowerCone(freq[2]/power))
+for i = 1:n-2
+    global power += freq[i+2]
+    @constraint(model, vcat(x[i+2],z[i],z[i+1]) in MOI.PowerCone(freq[i+2]/power))
+end
+# @constraint(model, vcat(gamma, A * x) in MOI.SecondOrderCone(n + 1))
+@constraint(model, vcat(gamma, A * x) in MOI.NormInfinityCone(n + 1))
+@constraint(model, vcat(sqrt(n) * gamma, A * x) in MOI.NormOneCone(n + 1))
+optimize!(model)
+
+#Result from Clarabel's generalized power cone
+println("generalized power cones via Clarabel")
+model = Model(Clarabel.Optimizer)
+@variable(model, t)
+@variable(model, x[1:n])
+@objective(model, Max, t)
+@constraint(model, vcat(x,t) in Clarabel.GenPowerConeT(freq,n,1))
+# @constraint(model, vcat(gamma, A * x) in MOI.SecondOrderCone(n + 1))
+@constraint(model, vcat(gamma, A * x) in MOI.NormInfinityCone(n + 1))
+@constraint(model, vcat(sqrt(n) * gamma, A * x) in MOI.NormOneCone(n + 1))
+optimize!(model)
+
+#Result from Hypatia
+println("generalized power cones via Hypatia")
+model = Model(Hypatia.Optimizer)
+@variable(model, t)
+@variable(model, x[1:n])
+@objective(model, Max, t)
+@constraint(model, vcat(x,t) in Hypatia.GeneralizedPowerCone(freq,1,false))
+# @constraint(model, vcat(gamma, A * x) in MOI.SecondOrderCone(n + 1))
+@constraint(model, vcat(gamma, A * x) in MOI.NormInfinityCone(n + 1))
+@constraint(model, vcat(sqrt(n) * gamma, A * x) in MOI.NormOneCone(n + 1))
+optimize!(model)
+println(solution_summary(model))
+
+#Result from Mosek
+println("3x3 cones by Mosek")
+model = Model(Mosek.Optimizer)
+@variable(model, p[1:n])
+@variable(model,q[1:n-1])
+@variable(model,r[1:n-2])
+@objective(model, Max, q[end])
+# trnasform a general power cone into a product of 3x3 power cones
+power = freq[1] + freq[2]
+@constraint(model, vcat(p[2],p[1],q[1]) in MOI.PowerCone(freq[2]/power))
+for i = 1:n-2
+    global power += freq[i+2]
+    @constraint(model, r[i] == q[i])
+    @constraint(model, vcat(p[i+2],r[i],q[i+1]) in MOI.PowerCone(freq[i+2]/power))
+end
+@constraint(model, vcat(gamma, A * p) in MOI.NormInfinityCone(n + 1))
+@constraint(model, vcat(sqrt(n) * gamma, A * p) in MOI.NormOneCone(n + 1))
+optimize!(model)
+solution_summary(model)
