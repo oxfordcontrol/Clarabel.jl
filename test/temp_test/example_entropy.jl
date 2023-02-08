da1bfaf30abecd0da8bceed1b85ec8a96b7d4202
Yuwen Chen <aaronchenyuwen@gmail.com>
2023-02-08 09:56:46 +0000

    example signomial

--- test/temp_test/example_entropy.jl
+++ test/temp_test/example_entropy.jl
@@ -6,71 +6,71 @@ using Clarabel
 # using Hypatia
 using BenchmarkTools
 
-d = 5000
-dim = 2*d+1
-p_init = rand(d)
-# p_init = ones(d)
-p_init = p_init./sum(p_init)
-p_init[1] += 1-sum(p_init)
+# d = 5000
+# dim = 2*d+1
+# p_init = rand(d)
+# # p_init = ones(d)
+# p_init = p_init./sum(p_init)
+# p_init[1] += 1-sum(p_init)
 
-#Result from three-dimensional exponential cones Clarabel
-println("three-dimensional exponential cones via Clarabel")
-model = Model(Clarabel.Optimizer)
-@variable(model, p[1:d])
-@variable(model, q[1:d])
-@variable(model, r[1:d])
-@variable(model, t)
-@objective(model, Min, t)
-@constraint(model, sum(q) == 1)
-@constraint(model, p .== p_init)
-@constraint(model, -sum(r) <= t)
-for i = 1:d
-    @constraint(model, vcat(r[i],q[i],p[i]) in MOI.ExponentialCone())
-end
-optimize!(model)
+# #Result from three-dimensional exponential cones Clarabel
+# println("three-dimensional exponential cones via Clarabel")
+# model = Model(Clarabel.Optimizer)
+# @variable(model, p[1:d])
+# @variable(model, q[1:d])
+# @variable(model, r[1:d])
+# @variable(model, t)
+# @objective(model, Min, t)
+# @constraint(model, sum(q) == 1)
+# @constraint(model, p .== p_init)
+# @constraint(model, -sum(r) <= t)
+# for i = 1:d
+#     @constraint(model, vcat(r[i],q[i],p[i]) in MOI.ExponentialCone())
+# end
+# optimize!(model)
 
-#Result from Clarabel
-println("entropy cones via Clarabel")
-model = Model(Clarabel.Optimizer)
-@variable(model, p[1:d])
-@variable(model, q[1:d])
-@variable(model, t)
-@objective(model, Min, t)
-@constraint(model, sum(q) == 1)
-@constraint(model, p .== p_init)
-@constraint(model, vcat(t,p,q) in Clarabel.EntropyConeT(dim))
-optimize!(model)
+# #Result from Clarabel
+# println("entropy cones via Clarabel")
+# model = Model(Clarabel.Optimizer)
+# @variable(model, p[1:d])
+# @variable(model, q[1:d])
+# @variable(model, t)
+# @objective(model, Min, t)
+# @constraint(model, sum(q) == 1)
+# @constraint(model, p .== p_init)
+# @constraint(model, vcat(t,p,q) in Clarabel.EntropyConeT(dim))
+# optimize!(model)
 
 
-#Result from three-dimensional exponential cones ECOS
-println("three-dimensional exponential cones via ECOS")
-model = Model(ECOS.Optimizer)
-@variable(model, p[1:d])
-@variable(model, q[1:d])
-@variable(model, r[1:d])
-@variable(model, t)
-@objective(model, Min, t)
-@constraint(model, sum(q) == 1)
-@constraint(model, p .== p_init)
-@constraint(model, -sum(r) <= t)
-for i = 1:d
-    @constraint(model, vcat(r[i],q[i],p[i]) in MOI.ExponentialCone())
-end
+# #Result from three-dimensional exponential cones ECOS
+# println("three-dimensional exponential cones via ECOS")
+# model = Model(ECOS.Optimizer)
+# @variable(model, p[1:d])
+# @variable(model, q[1:d])
+# @variable(model, r[1:d])
+# @variable(model, t)
+# @objective(model, Min, t)
+# @constraint(model, sum(q) == 1)
+# @constraint(model, p .== p_init)
+# @constraint(model, -sum(r) <= t)
+# for i = 1:d
+#     @constraint(model, vcat(r[i],q[i],p[i]) in MOI.ExponentialCone())
+# end
+# # @constraint(model, vcat(t,p,q) in MOI.RelativeEntropyCone(dim))
+# optimize!(model)
+
+
+# #Result from Mosek
+# println("entropy cones via Mosek")
+# model = Model(Mosek.Optimizer)
+# @variable(model, p[1:d])
+# @variable(model, q[1:d])
+# @variable(model, t)
+# @objective(model, Min, t)
+# @constraint(model, sum(q) == 1)
+# @constraint(model, p .== p_init)
 # @constraint(model, vcat(t,p,q) in MOI.RelativeEntropyCone(dim))
-optimize!(model)
-
-
-#Result from Mosek
-println("entropy cones via Mosek")
-model = Model(Mosek.Optimizer)
-@variable(model, p[1:d])
-@variable(model, q[1:d])
-@variable(model, t)
-@objective(model, Min, t)
-@constraint(model, sum(q) == 1)
-@constraint(model, p .== p_init)
-@constraint(model, vcat(t,p,q) in MOI.RelativeEntropyCone(dim))
-optimize!(model)
+# optimize!(model)
 
 # #Result from Hypatia
 # using Hypatia
@@ -89,12 +89,12 @@ optimize!(model)
 # # Another example from signomial and polynomial Optimization, 
 # # from https://github.com/chriscoey/Hypatia.jl/tree/master/examples/signomialmin
 # ############################################################
+include("signomial_test/JuMP.jl")
+instance = SignomialMinJuMP{Float64}(5,10)
 
-# instance = SignomialMinJuMP{Float64}(5,10)
+model = Model(Clarabel.Optimizer)
+model = build(instance,model)
+# model = clarabel_build(instance)      #using entropycone directly
 
-# # model = Model(Clarabel.Optimizer)
-# # model = build(instance,model)
-# model = clarabel_build(instance)
-
-# set_optimizer_attribute(model, "max_iter", 500)
-# optimize!(model)
+set_optimizer_attribute(model, "max_iter", 500)
+optimize!(model)
