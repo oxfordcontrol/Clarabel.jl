57568e1cade67e8ee9fcce55c8648031d32c5127
goulart-paul <paul.goulart@eng.ox.ac.uk>
2023-07-21 23:47:30 +0100

    improved handling of eq. constrained QP

--- src/info.jl
+++ src/info.jl
@@ -267,7 +267,7 @@ function _check_convergence(
     dinf_status::SolverStatus,
 ) where {T}
 
-    if info.ktratio < tol_ktratio && _is_solved(info, tol_gap_abs, tol_gap_rel, tol_feas)
+    if info.ktratio <= one(T) && _is_solved(info, tol_gap_abs, tol_gap_rel, tol_feas)
         info.status = solved_status
     elseif info.ktratio > 1000. / tol_ktratio
         if _is_primal_infeasible(info, residuals, tol_infeas_abs, tol_infeas_rel)
