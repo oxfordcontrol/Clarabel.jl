ba58118d27bb9759e554ca92b4d9c5620c06648d
Paul Goulart <paul.goulart@eng.ox.ac.uk>
2023-08-30 12:50:47 +0100

    fix LP style initialization (#134)
    * fix LP style initialization

--- src/kktsystem.jl
+++ src/kktsystem.jl
@@ -96,12 +96,13 @@ function kkt_solve_initial_point!(
 
     if iszero(nnz(data.P))
         # LP initialization
-        # solve with [0;b] as a RHS to get (x,s) initializers
+        # solve with [0;b] as a RHS to get (x,-s) initializers
         # zero out any sparse cone variables at end
         kktsystem.workx .= zero(T)
         kktsystem.workz .= data.b
         kktsolver_setrhs!(kktsystem.kktsolver, kktsystem.workx, kktsystem.workz)
         is_success = kktsolver_solve!(kktsystem.kktsolver, variables.x, variables.s)
+        variables.s .= -variables.s
 
         if !is_success return is_success end
 
